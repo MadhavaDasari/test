9a965c1d01b400df8e09de60ece9dcf67686dd6c
MadhavaDasari <dasarimadhava810@gmail.com>
2020-04-23 11:37:13 +0530

    print

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
 # python program to read an entire file 
 file = open("test.txt","r")     #giving the file object read mode 
-print(file.read())              # reading the entire file 
+           # reading the entire file 
+print(6)
